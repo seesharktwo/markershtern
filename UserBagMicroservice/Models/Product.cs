079d7109b64f913bf976142a467d76d45c6751e1
ctpukep7 <andrew.lila45@gmail.com>
2022-08-02 14:26:45 +0600

    Add KafkaConsumerService

--- UserBagMicroservice/Models/Product.cs
+++ UserBagMicroservice/Models/Product.cs
@@ -2,10 +2,15 @@
 
 namespace UserBagMicroservice.Models
 {
-    public class Product : Document
+    public class Product : Document, ICloneable
     {
         public string Name { get; set; }
         public string AuthorId { get; set; }
         public int Quantity { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
     }
 }
--- UserBagMicroservice/Program.cs
+++ UserBagMicroservice/Program.cs
@@ -11,16 +11,21 @@ builder.Services.AddGrpc();
 
 builder.Services.Configure<MongoDbSettings>(configuration.GetSection("MongoDbSettings"));
 
+builder.Services.Configure<KafkaConsumerSettings>(configuration.GetSection("KafkaConsumerSettings"));
+
 builder.Services.AddSingleton<IMongoDbSettings>(serviceProvider =>
     serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
 
 builder.Services.AddScoped(typeof(IMongoRepository<>), typeof(MongoRepository<>));
 
+builder.Services.AddSingleton<MongoRepository<UserBagMicroservice.Models.UserBag>>();
+
+builder.Services.AddHostedService<KafkaConsumerService>();
+
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
-app.MapGrpcService<GreeterService>();
 app.MapGrpcService<UserBriefcaseService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
