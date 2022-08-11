42196a661fc40ca1d595b1ab6cc72508d02f280a
Aleksandr <74893483+ted7007@users.noreply.github.com>
2022-08-11 16:09:58 +1000

    feat: relocate protos (#24)

---
+++ Facade/Facade/Facade/GrpcServices/GrpcOrderService.cs
@@ -0,0 +1,31 @@
+﻿using Facade.Services;
+using Grpc.Core;
+using OrdersClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Facade.GrpcServices
+{
+    public class GrpcOrderService : OrdersClient.OrdersClient.OrdersClientBase
+    {
+        private OrderService _orderService;
+
+        public GrpcOrderService(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        
+        
+
+        public override async Task<CreateOrderResponse> CreateOrder(CreateOrderRequest request, ServerCallContext context)
+        {
+            var mappedRequest = Mapper.Map<OrdersClient.CreateOrderRequest, Orders.CreateOrderRequest>(request);
+            (bool isComplite, Orders.CreateOrderResponse responce, Exception exception) creationResult =
+                        await _orderService.CreateOrderAsync(mappedRequest);
+            if (!creationResult.isComplite)
+                throw new RpcException(Status.DefaultCancelled, creationResult.exception.Message);
+            var mappedResponse = Mapper.Map<Orders.CreateOrderResponse, OrdersClient.CreateOrderResponse>(creationResult.responce);
+            return mappedResponse;
+        }
+    }
+}
