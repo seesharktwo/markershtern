60f451b11ae6d450742c388c369a4666fc4e4c83
Aleksandr <74893483+ted7007@users.noreply.github.com>
2022-08-17 14:54:02 +1000

    feat: update mapper (#30)

---
+++ Facade/Facade/Facade/MapperProfiles/OrderMapperConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MoneyTypes;
+
+namespace Facade.Mapper
+{
+    public class OrderMapperConfigurations : Profile
+    {
+        public OrderMapperConfigurations()
+        {
+            CreateMap<Order.CreateOrderRequest, OrderClient.CreateOrderRequest>();
+            CreateMap<OrderClient.CreateOrderRequest, Order.CreateOrderRequest>();
+            CreateMap<OrderClient.CreateOrderResponse, Order.CreateOrderResponse>();
+            CreateMap<Order.CreateOrderResponse, OrderClient.CreateOrderResponse>();
+            CreateMap<DecimalValue, decimal>().ConvertUsing(val => val);
+            CreateMap<decimal, DecimalValue>().ConvertUsing(val => val);
+        }
+    }
+}
