8545679642d8cc6dc2148f31e931c49f6e27e224
Александр Косилов <ted7007@yandex.ru>
2022-08-18 18:48:34 +1000

    Facade feat: add grpc services

--- Facade/Facade/Facade/MapperProfiles/OrderMapperConfigurations.cs
+++ Facade/Facade/Facade/MapperProfiles/OrderMapperConfigurations.cs
@@ -11,8 +11,6 @@ namespace Facade.Mapper
             CreateMap<OrderForClient.CreateOrderRequest, Order.CreateOrderRequest>();
             CreateMap<OrderForClient.CreateOrderResponse, Order.CreateOrderResponse>();
             CreateMap<Order.CreateOrderResponse, OrderForClient.CreateOrderResponse>();
-            CreateMap<DecimalValue, decimal>().ConvertUsing(val => val);
-            CreateMap<decimal, DecimalValue>().ConvertUsing(val => val);
         }
     }
 }
--- Facade/Facade/Facade/MapperProfiles/ProductMapperConfiguration.cs
+++ Facade/Facade/Facade/MapperProfiles/ProductMapperConfiguration.cs
@@ -11,8 +11,6 @@ namespace Facade.Mapper
             CreateMap<ProductForClient.GetProductsRequest, Product.GetProductsRequest>();
             CreateMap<ProductForClient.GetProductsResponse, Product.GetProductsResponse>();
             CreateMap<Product.GetProductsResponse, ProductForClient.GetProductsResponse>();
-            CreateMap<DecimalValue, decimal>().ConvertUsing(val => val);
-            CreateMap<decimal, DecimalValue>().ConvertUsing(val => val);
         }
 
     }
