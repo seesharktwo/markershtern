66100dedd6afe69fa72d1be2b953ef68bcdcda4c
Aleksandr <74893483+ted7007@users.noreply.github.com>
2022-08-20 00:50:55 +1000

    BalanceMicroservice feat:  update TopicBuilding (#34)
    * feat: update TopicBuilding
    
    * fix: update topicBuilder to object

--- UserBalanceMicroservice/UserBalanceMicroservice/UserBalanceMicroservice/Program.cs
+++ UserBalanceMicroservice/UserBalanceMicroservice/UserBalanceMicroservice/Program.cs
@@ -1,6 +1,8 @@
 using UserBalanceMicroservice.Configs;
 using UserBalanceMicroservice;
 using UserBalanceMicroservice.Services;
+using Microsoft.Extensions.Options;
+using UserBalanceMicroservice.KafkaServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,11 +23,14 @@ builder.Services.AddSingleton<BalanceOperationService>();
 builder.Services.AddHostedService<KafkaConsumerService>();
 
 
-AdminTopickBuilderService.Build();
+
 var app = builder.Build();
+
+var topicService = new AdminTopicBuilderService(app);
+topicService.TopicsBuildAsync();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<BalanceServiceGrpc>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+    
 app.Run();
