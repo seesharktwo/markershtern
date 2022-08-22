725c5df7e0d188c9ada71a27f423a85bbc51fc54
stictt <stic_t@mail.ru>
2022-08-22 08:38:55 +0300

    Create: New service Balance (#26)
    * Create: New service Balance
    
    * Fix: Fix bad code
    
    * BalanceMicroservice feat:  update TopicBuilding (#34)
    
    * feat: update TopicBuilding
    
    * fix: update topicBuilder to object
    
    * fix: protos update
    
    * Delete .vs directory
    
    * fix: add logger in DI
    
    * fix: some fixes
    
    * fix: update port
    
    Co-authored-by: Aleksandr <74893483+ted7007@users.noreply.github.com>
    Co-authored-by: Александр Косилов <ted7007@yandex.ru>

---
+++ UserBalanceMicroservice/UserBalanceMicroservice/UserBalanceMicroservice/Services/BalanceServiceGrpc.cs
@@ -0,0 +1,31 @@
+using Grpc.Core;
+using UserBalanceMicroservice;
+using Protos.CustomTypes;
+using MoneyTypes;
+using AutoMapper;
+using UserBalanceMicroservice.Models;
+namespace UserBalanceMicroservice.Services
+{
+    public class BalanceServiceGrpc : BalanceService.BalanceServiceBase
+    {
+        private readonly ILogger<BalanceServiceGrpc> _logger;
+        private readonly BalanceContext _balanceContext;
+        public BalanceServiceGrpc(ILogger<BalanceServiceGrpc> logger, BalanceContext balanceContext)
+        {
+            _logger = logger;
+            _balanceContext = balanceContext;
+        }
+
+        public async override Task<GetBalanceResponse> GetBalance(GetBalanceRequest request, ServerCallContext context)
+        {
+            var user = await _balanceContext.GetBalanceAsync(request.Id);
+
+            return
+                new GetBalanceResponse()
+                {
+                    UserId = user.Id,
+                    Balance = DecimalValue.FromDecimal(user.Money)
+                };
+        }
+    }
+}
