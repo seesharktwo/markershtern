e64ce3f915468226e56f5708fb8c938dd774585c
Александр Косилов <ted7007@yandex.ru>
2022-08-23 18:12:04 +1000

    fix: fix fail with mongo

--- infrastructure/scripts/mongo_setup.sh
+++ infrastructure/scripts/mongo_setup.sh
@@ -3,6 +3,9 @@
 MONGODB1=mongo1
 MONGODB2=mongo2
 MONGODB3=mongo3
+MONGODB4=mongo4
+MONGODB5=mongo5
+MONGODB6=mongo6
 
 echo "**********************************************"
 
@@ -23,6 +26,21 @@ var cfg = {
         {
             "_id": 2,
             "host": "${MONGODB3}:27017",
+            "priority": 0
+        }
+        {
+            "_id": 3,
+            "host": "${MONGODB4}:27017",
+            "priority": 0
+        }
+        {
+            "_id": 4,
+            "host": "${MONGODB5}:27017",
+            "priority": 0
+        }
+        {
+            "_id": 5,
+            "host": "${MONGODB6}:27017",
             "priority": 0
         }
     ]
