a8f099b1c9c5a8ac301a3e9b7824f0b2a694b8e8
Александр Косилов <ted7007@yandex.ru>
2022-08-24 13:14:31 +1000

    feat: create protodocs

---
+++ docusaurusService/website/sidebars.js
@@ -0,0 +1,25 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'What is Protobuffet?',
+      collapsed: false,
+      items: [
+        'what/overview',
+        'what/features',
+        'what/demo',
+        'what/motivation',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'How do we use it?',
+      collapsed: false,
+      items: [
+        'how/packages',
+        'how/installation',
+        'how/usage',
+      ],
+    },
+  ],
+};
